2779421dab15afff6d4620eba11b294666ac3107
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2025-05-19 04:21:37 +0100

    project

---
+++ laradesk-1.1.2/app/Support/Setting.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Support;
+
+use dacoto\SetEnv\SetEnv as SetEnvSetEnv;
+use SetEnv;
+
+/**
+ * Class Setting
+ * @package App\Support
+ */
+class Setting
+{
+    public static function get($key, $default = null)
+    {
+        if ($setting = \App\Models\Setting::find($key)) {
+            return $setting->value;
+        }
+        return $default;
+    }
+
+    public static function setEnv($key, $value = null): void
+    {
+        SetEnv::setKey($key, $value);
+        SetEnv::save();
+    }
+}
